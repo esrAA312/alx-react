f4e556548ceb1d85ddee7113a2e90d623ccdddbc
esrAA312 <138700352+esrAA312@users.noreply.github.com>
2024-07-23 17:36:26 +0300

    done

---
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Notifications } from './Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="root-app">
+      <App />
+    </div>
+    <div id="root-notifications">
+      <Notifications />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
